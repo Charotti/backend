62b7ca34085e1e895eeef7be167c9a43382fcaff
unknown <clamy6@yahoo.fr>
2022-03-09 17:29:54 +0100

    routers

--- chapitre_04/Validation/index.js
+++ chapitre_04/Validation/index.js
@@ -1,49 +1,15 @@
 const express = require("express");
-const router = express.Router();
+
 const app = express();
-app.use(express.json());
 const Joi = require("Joi");
+const usersRouter = require("./router");
 
-const schema = Joi.object({
-  userName: Joi.string().min(4).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  age: Joi.number().min(10).max(99).required(),
-  city: Joi.string().required(),
+app.use(express.json());
+app.use("/", usersRouter);
+
+app.use("*", (err, req, res, next) => {
+  res.send("errror");
 });
-const users = [
-  {
-    userName: "Caroline",
-    email: "caroline@gmail.com",
-    age: 25,
-    city: "Carennac",
-  },
-];
-
-// ROUTES
-
-router.get("/", (_req, res) => {
-  res.json(users);
-});
-router.post("/", (req, res) => {
-  const user = req.body;
-
-  const validationResult = schema.validate(user);
-
-  if (validationResult.error) {
-    return res.status(400).json({
-      message: validationResult.error,
-    });
-  }
-  users.push(user);
-  res.json({ message: "User has been added", users });
-});
-router.get("/users/:userName", (req, res) => {
-  res.send("usersName" + req.params.userName);
-});
-
 app.listen(8000, () => {
   console.log("server started en port: ");
 });
-module.exports = router;
---
+++ chapitre_04/Validation/router.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const Joi = require("Joi");
+
+const schema = Joi.object({
+  userName: Joi.string().min(4).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  age: Joi.number().min(10).max(99).required(),
+  city: Joi.string().required(),
+});
+const users = [
+  {
+    userName: "Caroline",
+    email: "caroline@gmail.com",
+    age: 25,
+    city: "Carennac",
+  },
+];
+// / ROUTES
+
+router.get("/", (_req, res) => {
+  res.json(users);
+});
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  const validationResult = schema.validate(user);
+
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error,
+    });
+  }
+  users.push(user);
+  res.json({ message: "User has been added", users });
+});
+router.get("/users/:userName", (req, res) => {
+  const user = users.find((user) => {
+    return (
+      user.userName.replace(" ", "-").toLowerCase() ===
+      req.params.userName.replace(" ", "-").toLowerCase()
+    );
+  });
+  if (!user) {
+    return res.send("User not found");
+  }
+  res.json(user);
+});
+module.exports = router;
