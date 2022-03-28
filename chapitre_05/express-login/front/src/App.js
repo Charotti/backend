ec7a9f043849ecb7b0a42130b88ebf61a52259df
unknown <clamy6@yahoo.fr>
2022-03-28 15:58:54 +0200

    navbar et routes

--- chapitre_05/express-login/front/src/App.js
+++ chapitre_05/express-login/front/src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+import Admin from "./pages/Admin";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Login</Link>
+          <Link to="/admin">Admin</Link>
+          <Link to="/signUp">SignUp</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/admin" component={Admin} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
